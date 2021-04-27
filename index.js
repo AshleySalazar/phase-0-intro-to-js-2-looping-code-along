8f259f06aeca22378202f58b7b7f11016ae50230
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-04-27 16:19:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown(num){
+    let i = num;
+    while ( i >= 0 ){
+        console.log (i--);
+    }
+
+}
+
+function writeCards(names){
+    let letters = []
+    for ( let i = 0; i <names.length; i ++){
+      letters.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    }
+    return letters
+}
